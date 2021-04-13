102a9968cdf2a536ec759256719daa2f59d470ee
jonathan <jonathan.henry@epfl.ch>
2021-04-13 09:26:41 +0200

    projet_init

---
+++ Projet_Robotique/process_image.h
@@ -0,0 +1,9 @@
+#ifndef PROCESS_IMAGE_H
+#define PROCESS_IMAGE_H
+
+float get_distance_cm(void);
+void process_image_start(void);
+uint16_t extract_line_width(uint8_t *buffer);
+uint16_t get_line_position(void);
+
+#endif /* PROCESS_IMAGE_H */
