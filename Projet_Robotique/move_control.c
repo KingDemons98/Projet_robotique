f5a9a0ce5fd5606957d0c6ee2ba2e8d3a61a1c86
jonathan <jonathan.henry@epfl.ch>
2021-04-15 12:40:09 +0200

    leger update move

--- Projet_Robotique/move_control.c
+++ Projet_Robotique/move_control.c
@@ -1,2 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
 #include <move_control.h>
+#include <proximity.h>
+#include <motors.h>
+#include <camera/po8030.h>
+
+
+// module de gestion du déplacement avec la caméra et les capteurs de proximité
 
