a4d36ad29064b2f116ce2fbf9c10948225471a34
jonathan <jonathan.henry@epfl.ch>
2021-04-30 14:50:24 +0200

    legere modif move (added semaphore for control)

--- Projet_Robotique/move_control.c
+++ Projet_Robotique/move_control.c
@@ -15,14 +15,15 @@
 
 // module de gestion du déplacement avec la caméra et les capteurs de proximité
 
+static BSEMAPHORE_DECL(block_passed, TRUE);				//permet de signaler quand le bloc est dépassé
+static BSEMAPHORE_DECL(position_reached, TRUE);			//signale quand le robot est a la bonne distance du prochain bloc
+
 static THD_WORKING_AREA(waMoveControl, 256);
 static THD_FUNCTION(MoveControl, arg)
 {
 	chRegSetThreadName(__FUNCTION__);
 	(void)arg;
 	systime_t time;
-
-
 }
 
 void move_control_start(void)
